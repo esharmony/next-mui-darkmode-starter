db3e671d80d61da9b4c8abda197010d576ea4d54
Matthew Barnden <development.esharmony@gmail.com>
2021-12-10 00:56:20 +0000

    better typing

--- helpers/index.ts
+++ helpers/index.ts
@@ -1,6 +1,6 @@
 import cookie from 'cookie';
+import { IncomingMessage } from 'http';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function parseCookies(req: any) {
+export function parseCookies(req: IncomingMessage) {
   return cookie.parse(req ? req.headers.cookie || '' : document.cookie);
 }
